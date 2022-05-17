f4606c41bc3816d8b4314f1e740762b6b1386467
Eduardo Hernandez <edhernandez@santandertecnologia.com.ar>
2022-05-17 20:22:14 -0300

    initial commit

---
+++ src/main/java/com/edh/mendelexam/MendelExamApplication.java
@@ -0,0 +1,13 @@
+package com.edh.mendelexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendelExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MendelExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edh/mendelexam/MendelExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edh.mendelexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendelExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
