55e58a600edeff1cd7b3bde88ee97c659b1cadc5
Eduardo Hernandez <edhernandez@santandertecnologia.com.ar>
2022-05-19 08:35:44 -0300

    add impl and test for GetIdByTypeUseCase and tests for it

---
+++ src/main/java/com/edh/mendelexam/business/usecases/impl/GetIdByTypeUseCaseImpl.java
@@ -0,0 +1,29 @@
+package com.edh.mendelexam.business.usecases.impl;
+
+import com.edh.mendelexam.business.exception.NotSuchTypeException;
+import com.edh.mendelexam.business.usecases.GetIdByTypeUseCase;
+import com.edh.mendelexam.ports.TransactionNodePort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+public class GetIdByTypeUseCaseImpl implements GetIdByTypeUseCase {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private TransactionNodePort transactionNodePort;
+
+    public GetIdByTypeUseCaseImpl(TransactionNodePort transactionNodePort) {
+        this.transactionNodePort = transactionNodePort;
+    }
+
+    @Override
+    public Set<Long> execute(String type) {
+        Set<Long> result = transactionNodePort.getIdByType(type);
+        if (result.isEmpty()) {
+            logger.error("Cannot found ids for type {}", type);
+            throw new NotSuchTypeException();
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/edh/mendelexam/unit/business/usecase/GetIdByTypeUseCaseTest.java
@@ -0,0 +1,50 @@
+package com.edh.mendelexam.unit.business.usecase;
+
+import com.edh.mendelexam.business.exception.NotSuchParentException;
+import com.edh.mendelexam.business.exception.NotSuchTypeException;
+import com.edh.mendelexam.business.usecases.GetIdByTypeUseCase;
+import com.edh.mendelexam.business.usecases.impl.CreateTransactionUseCaseImpl;
+import com.edh.mendelexam.business.usecases.impl.GetIdByTypeUseCaseImpl;
+import com.edh.mendelexam.ports.TransactionNodePort;
+
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+public class GetIdByTypeUseCaseTest {
+
+    TransactionNodePort transactionNodePort = mock(TransactionNodePort.class);
+    GetIdByTypeUseCase getIdByTypeUseCase = new GetIdByTypeUseCaseImpl(transactionNodePort);
+
+    @Test
+    void execute_whenExistsTypeWithIds_mustReturnSetOfIds() {
+        String type = "type";
+        HashSet<Long> ids = Sets.newHashSet(1L, 2L);
+        when(transactionNodePort.getIdByType(type)).thenReturn(ids);
+        assertThat(getIdByTypeUseCase.execute(type)).containsAll(ids);
+        verify(transactionNodePort).getIdByType(type);
+    }
+
+    @Test
+    void execute_whenNonExistsType_mustThrowException() {
+        Logger logger = mock(Logger.class);
+        try (MockedStatic<LoggerFactory> loggerMockedStatic = Mockito.mockStatic(LoggerFactory.class)) {
+            loggerMockedStatic.when(() -> LoggerFactory.getLogger(GetIdByTypeUseCaseImpl.class)).thenReturn(logger);
+            when(transactionNodePort.getIdByType(any())).thenReturn(new HashSet<>());
+            GetIdByTypeUseCase getIdByTypeUseCase = new GetIdByTypeUseCaseImpl(transactionNodePort);
+            assertThatThrownBy(() -> getIdByTypeUseCase.execute("anyType"))
+                    .isInstanceOf(NotSuchTypeException.class);
+            verify(transactionNodePort).getIdByType("anyType");
+            verify(logger).error("Cannot found ids for type {}", "anyType");
+        }
+    }
+}
