110fa1e1910ad48f795e1807a9b8eac94efa18b9
Eduardo Hernandez <edhernandez@santandertecnologia.com.ar>
2022-05-20 07:38:05 -0300

    return to old impl of controller advice

--- src/main/java/com/edh/mendelexam/api/controllers/ExceptionControllerAdvice.java
+++ src/main/java/com/edh/mendelexam/api/controllers/ExceptionControllerAdvice.java
@@ -4,12 +4,9 @@ import com.edh.mendelexam.api.dtos.responses.ApiError;
 import com.edh.mendelexam.api.dtos.responses.TransactionResponseDto;
 import com.edh.mendelexam.business.exception.NoSuchParentException;
 import com.edh.mendelexam.business.exception.NoSuchTypeException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.NoSuchElementException;
 
@@ -17,21 +14,18 @@ import java.util.NoSuchElementException;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(NoSuchParentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public TransactionResponseDto handleNoSuchParentException(NoSuchParentException ex) {
-        return TransactionResponseDto.error(ex.getMessage());
+    public ResponseEntity<TransactionResponseDto> handleNoSuchParentException(NoSuchParentException ex) {
+        return ResponseEntity.badRequest().body(TransactionResponseDto.error(ex.getMessage()));
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleNoSuchElementException(NoSuchElementException ex) {
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(NoSuchTypeException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleNoSuchTypeException(NoSuchTypeException ex) {
-
+    public ResponseEntity<?> handleNoSuchTypeException(NoSuchTypeException ex) {
+        return ResponseEntity.notFound().build();
     }
 
 }
